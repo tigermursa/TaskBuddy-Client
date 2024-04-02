a08451cd432f835d8c98f9cea6514b26fc7221f3
Mursalin Hossain <mursalin2311@gmail.com>
2024-04-03 02:49:05 +0600

    "added notepad on the drawer"

--- src/Components/ui/Drawer/Drawer.tsx
+++ src/Components/ui/Drawer/Drawer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Drawer } from "antd";
 import CalendarAnt from "../Calender/Calender";
+import Notepad from "../Notepad/Notepad";
 
 interface DrawerAntProps {
   open: boolean;
@@ -11,6 +12,7 @@ const DrawerAnt: React.FC<DrawerAntProps> = ({ open, onClose }) => {
   return (
     <Drawer title="Make best 2024" onClose={onClose} visible={open}>
       <CalendarAnt />
+      <Notepad />
     </Drawer>
   );
 };
--- src/Components/ui/Notepad/Notepad.tsx
+++ src/Components/ui/Notepad/Notepad.tsx
@@ -1,11 +1,39 @@
-
+import { EditOutlined } from "@ant-design/icons";
+import { useState, useEffect } from "react";
 
 const Notepad = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [text, setText] = useState("");
+
+  // Load text from local storage when component mounts
+  useEffect(() => {
+    const savedText = localStorage.getItem("notepadText");
+    if (savedText) {
+      setText(savedText);
+    }
+  }, []);
+
+  const handleChange = (e: { target: { value: string } }) => {
+    const newText = e.target.value;
+    setText(newText);
+    // Save text to local storage
+    localStorage.setItem("notepadText", newText);
+  };
+
+  return (
+    <div className="max-w-lg mx-auto mt-8 p-4 bg-gray-50 rounded-md">
+      <h1 className="text-xl font-semibold mb-4">
+        Notepad
+        <EditOutlined className="ms-2" />
+      </h1>
+      <textarea
+        placeholder="Write your note here..."
+        className="w-full p-2 mb-4  rounded-md ubuntu-regular-italic text-lg"
+        rows={20} // Adjust the number of rows as needed
+        value={text}
+        onChange={handleChange}
+      ></textarea>
+    </div>
+  );
 };
 
 export default Notepad;
